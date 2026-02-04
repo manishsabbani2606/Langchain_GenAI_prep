077ffc17c7371f27d8c013f1992fb6efc53b87b7
manishsabbani2606 <sabbanimanish2606@gmail.com>
2026-02-03 19:58:50 -0500

    RAG YT ran both ingestion & Retrieval Pipeline

--- RAG YT/retrieval_pipeline.py
+++ RAG YT/retrieval_pipeline.py
@@ -1,25 +1,32 @@
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-persist_directory="db/chroma_dbvector_store"
+persist_directory = "db/chroma_dbvector_store"
 
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
 
-db = Chroma(persist_directory=persist_directory,
-            embedding_function=embedding_model,collection_metadata={"hnsw:space":"cosine"})
-
+db = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw:space": "cosine"}
+)
 
 query = "How much did Microsoft pay to acquire GitHub?"
 
-retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":3})
+retriever = db.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
 
 docs = retriever.invoke(query)
 
-print(f"Top 3 most similar documents to the query '{query}':\n")
+print(f"\nTop {len(docs)} most similar documents to the query:\n")
 
-for i, doc in enumerate(docs,1):
-    print(f"Document {i+1} content preview: {doc.page_content}...\n")
-    
+for i, doc in enumerate(docs, 1):
+    print(f"--- Document {i} ---")
+    print(doc.page_content)
+    print("Metadata:", doc.metadata)
+    print()
